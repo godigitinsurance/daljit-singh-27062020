34bdb6c23c142db7a87949f1860f334e79ae5cdb
godigitinsurance <59437973+godigitinsurance@users.noreply.github.com>
2020-06-26 10:28:45 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,13 @@
+$("#customerName").text("Daljit Singh");
+$("#policyNumber").text("VCV2026926");
+$("#startDate").text("27-Jun-2020");
+$("#endDate").text("26-Jun-2021");
+
+
+$("#type").text("Two Wheeler");
+$("#registrationNumber").text("PB07Y0246");
+$("#make").text("HERO HONDA");
+$("#model").text("SPLENDOR PLUS");
+$("#vehicalSubType").text("DRUM BRAKE");
+$("#engineNo").text("AHB15616");
+$("#chassis").text("AHB24606");
